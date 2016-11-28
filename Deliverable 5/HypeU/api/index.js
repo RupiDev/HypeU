13931139ba0ee5c5af0a7048fb5591c43b01cc6c
Anirudh B <anidev.aelico@gmail.com>
2016-11-28 07:41:49 +0000

    fixed more bugs

--- Deliverable 5/HypeU/api/index.js
+++ Deliverable 5/HypeU/api/index.js
@@ -1,8 +1,8 @@
 // Implements REST api
 
-module.exports.runRestServer = runRestServer
+module.exports.runRestServer = runRestServer;
 
-var database = require("./database")
+var database = require("./database");
 
 var restify = require("restify");
 
@@ -19,8 +19,8 @@ function runRestServer(port) {
     database.sequelize.sync().then(() => {
         server.listen(port, () => {
             console.log('rest server running on port %s', port);
-        })
-    })
+        });
+    });
 }
 
 // this method will return a promise, and in order to get the
@@ -47,9 +47,9 @@ function eventGetter(req, res, next)
 {
     // first need to make a query
     // then you are getting 
-    University.find({
+    database.University.find({
         where: {
-            universityID: req.param.universityID
+            universityID: req.params.universityID
         }
     }).then(function(university) {
         university.queryAllEvents().then(function(events) {
@@ -61,7 +61,7 @@ function eventGetter(req, res, next)
         }).catch(function(error){
             console.log("Error is received by querying for all the events based one University");
             res.send(400);
-        })
+        });
     })
     .catch(function(error){
         console.log("Error is received by getting University ID");
@@ -79,7 +79,7 @@ function creatingOrganization(req, res, next)
     promise.then(function(user) 
     {
          user.creatingOrganization("testToken", "testOrgName");
-         res.json({success: 1, orgID: 'testOrgID'})
+         res.json({success: 1, orgID: 'testOrgID'});
          res.send(200); // this indicates success
          next();
     })
@@ -103,13 +103,13 @@ function creatingEvent(req, res, next)
     {
         user.createEvent("testToken", "testOrgId", "testName", "testDescription", "testDate");
         res.json({success: 1, eventID: 'testEventID'});
-        res.send(200) // success  
+        res.send(200); // success  
     })
     .catch(function(error)
     {
         console.log("There has been an error in creating an event");
         res.json({success: 0, eventID: "testEventID"});
-        res.send(400) // error
+        res.send(400); // error
     });
 }
 server.post('/events/create', creatingEvent);
@@ -143,7 +143,7 @@ function addingAdmins(res, req, next)
         res.send(200);
     })
     .catch(function(error){
-        console.log("There has been an error in adding the specified user as an admin")
+        console.log("There has been an error in adding the specified user as an admin");
         res.json({success: 0, authToken: 'testAuthToken'});
         res.send(200);
     });
@@ -153,15 +153,17 @@ server.put("/orgs/ID/admins/add", addingAdmins);
 
 // POST
 // This method is used to login, basically used as a dummy method
-// Returns a 
+// Returns a json object with success and authToken
 function logIn(req, res, next)
 {
-    res.json({success: 1, authToken: 'testAuthToken'})
+    res.json({success: 1, authToken: 'testAuthToken'});
     // dummy method for log in
 }
 server.post('/login', logIn);
 
 // POST
+// This method is used to logout, basically used as a dummy method
+// Returns a json object with true
 function logOut(req, res, next)
 {
     // dummy method for log out
